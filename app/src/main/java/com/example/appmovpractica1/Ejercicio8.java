f3bc1972bbf354aee445d2ec656cdcae89eaefce
Roberto <robertomdzvz20@gmail.com>
2024-05-13 16:31:54 -0500

    Avances AppMovPractica1: UI y Backend Ejercicio 8

--- app/src/main/java/com/example/appmovpractica1/Ejercicio8.java
+++ app/src/main/java/com/example/appmovpractica1/Ejercicio8.java
@@ -1,6 +1,10 @@
 package com.example.appmovpractica1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Ejercicio8 extends AppCompatActivity {
+import java.util.Locale;
 
+public class Ejercicio8 extends AppCompatActivity {
+    //Declarar variables
+    EditText id_cnumero_ingreso;
+    TextView id_ctexto_resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +28,22 @@ public class Ejercicio8 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        //Asignar variables por id
+        id_cnumero_ingreso = findViewById(R.id.id_numero_ingreso);
+        id_ctexto_resultado = findViewById(R.id.id_texto_resultado);
+    }
+    public void calcular_cubo(View view){
+        //Verificar si los campos están vacíos
+        String numero_ingreso_str = id_cnumero_ingreso.getText().toString();
+        if (numero_ingreso_str.isEmpty()) {
+            Toast.makeText(this, "Ingrese un numero", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        double numero_elevar = Double.parseDouble(numero_ingreso_str);
+        double al_cubo = Math.pow(numero_elevar, 3);
+
+        String numero_al_cubo = String.format(Locale.US, "%.2f", al_cubo);
+        id_ctexto_resultado.setText(numero_al_cubo);
     }
 }
