63c07a847f5310c29b57a4f87a5c0d4ba64e387e
Roberto <robertomdzvz20@gmail.com>
2024-05-13 14:12:14 -0500

    Avances AppMovPractica1: UI y Backend Ejercicio 6

--- app/src/main/java/com/example/appmovpractica1/Ejercicio6.java
+++ app/src/main/java/com/example/appmovpractica1/Ejercicio6.java
@@ -1,6 +1,10 @@
 package com.example.appmovpractica1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Ejercicio6 extends AppCompatActivity {
 
+    //Declarar variables
+    EditText id_cnumero_verificar;
+    TextView id_ctexto_resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +27,27 @@ public class Ejercicio6 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Asignar variables por id
+        id_cnumero_verificar = findViewById(R.id.id_numero_verificar);
+        id_ctexto_resultado = findViewById(R.id.id_texto_resultado);
+    }
+
+    //Función Verificar número
+    public void verificar_numero (View view){
+        String numero_str = id_cnumero_verificar.getText().toString();
+        if (!numero_str.isEmpty()) {
+            int numero = Integer.parseInt(numero_str);
+            if (numero > 0) {
+                id_ctexto_resultado.setText("Positivo");
+            } else if (numero < 0) {
+                id_ctexto_resultado.setText("Negativo");
+            } else {
+                id_ctexto_resultado.setText("Cero");
+            }
+        } else {
+            Toast.makeText(this, "Ingrese un número", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
