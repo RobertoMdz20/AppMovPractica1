c8c28650906a9575020976609b084d520f4cac04
Roberto <robertomdzvz20@gmail.com>
2024-05-13 01:37:32 -0500

    Avances AppMovPractica1: Backend Ejercicio 10

--- app/src/main/java/com/example/appmovpractica1/Ejercicio10.java
+++ app/src/main/java/com/example/appmovpractica1/Ejercicio10.java
@@ -1,6 +1,10 @@
 package com.example.appmovpractica1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +12,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class Ejercicio10 extends AppCompatActivity {
+    //Declarar variables
+    private EditText id_ctiempo_recorrido, id_ctiempo_espera;
+    private TextView id_ctexto_resultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,33 @@ public class Ejercicio10 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Asinar variables por id
+        id_ctiempo_recorrido = findViewById(R.id.id_tiempo_recorrido);
+        id_ctiempo_espera = findViewById(R.id.id_tiempo_espera);
+        id_ctexto_resultado = findViewById(R.id.id_texto_resultado);
+    }
+    //Función Calcular
+    public void calcular_pago (View view){
+        //Verificar si los campos están vacíos
+        String tiempo_recorrido_str = id_ctiempo_recorrido.getText().toString();
+        String tiempo_espera_str = id_ctiempo_espera.getText().toString();
+        if (tiempo_recorrido_str.isEmpty() || tiempo_espera_str.isEmpty()) {
+            Toast.makeText(this, "Ingrese el tiempo de recorrido y de espera", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Double cm = Double.parseDouble(tiempo_recorrido_str);
+        Double ce = Double.parseDouble(tiempo_espera_str);
+        //Verificar si los campos son cero
+        if (cm == 0 && ce == 0) {
+            Toast.makeText(this, "El tiempo de recorrido y de espera no pueden ser cero", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        double arranque = 0.50;
+        double valorminuto = 0.18;
+        double valorespera = 0.12;
+        double valortotal = (arranque + (cm * valorminuto) + (ce * valorespera));
+        String totaltaxi = String.format(Locale.US, "%.2f", valortotal);
+        id_ctexto_resultado.setText(totaltaxi);
     }
 }
