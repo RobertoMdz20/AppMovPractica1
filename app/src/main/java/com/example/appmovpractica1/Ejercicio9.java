56b073becfd1c18fa96009137c5191e52c36da27
Roberto <robertomdzvz20@gmail.com>
2024-05-14 01:05:51 -0500

    Avances AppMovPractica1: UI y Backend Ejercicio 9

--- app/src/main/java/com/example/appmovpractica1/Ejercicio9.java
+++ app/src/main/java/com/example/appmovpractica1/Ejercicio9.java
@@ -1,6 +1,10 @@
 package com.example.appmovpractica1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +12,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Locale;
+
 public class Ejercicio9 extends AppCompatActivity {
 
+    //Declarar variables
+    EditText id_cingreso1, id_cingreso2, id_cingreso3;
+    TextView id_ctexto_resultado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +29,29 @@ public class Ejercicio9 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //Asignar variables por id
+        id_cingreso1 = findViewById(R.id.id_ingreso1);
+        id_cingreso2 = findViewById(R.id.id_ingreso2);
+        id_cingreso3 = findViewById(R.id.id_ingreso3);
+        id_ctexto_resultado = findViewById(R.id.id_texto_resultado);
+    }
+    //Función Calcular pago
+    public void calcular_promedio (View view){
+        //Verificar si los campos están vacíos
+        String id_ingreso1_str = id_cingreso1.getText().toString();
+        String id_ingreso2_str = id_cingreso2.getText().toString();
+        String id_ingreso3_str = id_cingreso3.getText().toString();
+        if (id_ingreso1_str.isEmpty() || id_ingreso2_str.isEmpty() || id_ingreso3_str.isEmpty()) {
+            Toast.makeText(this, "Ingrese los números", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        double ingreso1 = Double.parseDouble(id_ingreso1_str);
+        double ingreso2 = Double.parseDouble(id_ingreso2_str);
+        double ingreso3 = Double.parseDouble(id_ingreso3_str);
+        double calculo_promedio = (ingreso1+ingreso2+ingreso3)/3;
+        String promedio = String.format(Locale.US, "%.2f", calculo_promedio);
+        id_ctexto_resultado.setText(promedio);
+
     }
 }
